06cb31b69fd24770063bf639aea0d81d1219047a
Augusto Fraga Giachero <augustofg96@gmail.com>
2020-09-02 09:54:07 -0300

    Run rustfmt to conform with rust style

--- src/main.rs
+++ src/main.rs
@@ -21,42 +21,55 @@
  *
  ****************************************************************************/
 
-extern crate ddc_hi;
 extern crate clap;
+extern crate ddc_hi;
 extern crate parse_int;
 
+use clap::{App, AppSettings, Arg, SubCommand};
 use ddc_hi::{Ddc, Display};
-use clap::{Arg, App, AppSettings, SubCommand};
 use parse_int::parse;
 
 fn main() {
-    let matches = App::new("ddc-control").version("0.1.0")
+    let matches = App::new("ddc-control")
+        .version("0.1.0")
         .author("Augusto F. Giachero <afg@augustofg.net>")
         .about("Set / Get VCP parameters from connected monitors via DDC")
-        .arg(Arg::with_name("monitor-number")
-             .required(true)
-             .short("m")
-             .long("monitor-number")
-             .value_name("NUM")
-             .help("Monitor number")
-             .takes_value(true))
+        .arg(
+            Arg::with_name("monitor-number")
+                .required(true)
+                .short("m")
+                .long("monitor-number")
+                .value_name("NUM")
+                .help("Monitor number")
+                .takes_value(true),
+        )
         .setting(AppSettings::SubcommandRequired)
-        .subcommand(SubCommand::with_name("get-vcp")
-                    .about("Get the VCP feature value")
-                    .arg(Arg::with_name("FEATURE")
-                         .help("VCP feature number")
-                         .index(1)
-                         .required(true)))
-        .subcommand(SubCommand::with_name("set-vcp")
-                    .about("Set the VCP feature value")
-                    .arg(Arg::with_name("FEATURE")
-                         .help("VCP feature number")
-                         .index(1)
-                         .required(true))
-                    .arg(Arg::with_name("VALUE")
-                         .help("VCP feature value to set")
-                         .index(2)
-                         .required(true)))
+        .subcommand(
+            SubCommand::with_name("get-vcp")
+                .about("Get the VCP feature value")
+                .arg(
+                    Arg::with_name("FEATURE")
+                        .help("VCP feature number")
+                        .index(1)
+                        .required(true),
+                ),
+        )
+        .subcommand(
+            SubCommand::with_name("set-vcp")
+                .about("Set the VCP feature value")
+                .arg(
+                    Arg::with_name("FEATURE")
+                        .help("VCP feature number")
+                        .index(1)
+                        .required(true),
+                )
+                .arg(
+                    Arg::with_name("VALUE")
+                        .help("VCP feature value to set")
+                        .index(2)
+                        .required(true),
+                ),
+        )
         .get_matches();
 
     let monitor = match parse::<usize>(matches.value_of("monitor-number").unwrap()) {
@@ -64,50 +77,56 @@ fn main() {
         Err(e) => {
             eprintln!("Invalid argument for -m / --monitor: {}", e);
             std::process::exit(1)
-        },
+        }
     };
-
 
     if monitor < 1 {
         eprintln!("Monitor number should be greater than zero.");
         std::process::exit(1);
     }
 
-    let (feature, value, write) =
-        if let Some(feature_arg) = matches.subcommand_matches("get-vcp") {
-            (match parse::<u8>(feature_arg.value_of("FEATURE").unwrap()) {
+    let (feature, value, write) = if let Some(feature_arg) = matches.subcommand_matches("get-vcp") {
+        (
+            match parse::<u8>(feature_arg.value_of("FEATURE").unwrap()) {
                 Ok(num) => num,
                 Err(e) => {
                     eprintln!("Invalid argument for get-vcp: {}", e);
                     std::process::exit(1)
-                },
-            }, 0u16, false)
-        } else if let Some(feature_arg) = matches.subcommand_matches("set-vcp") {
-            (match parse::<u8>(feature_arg.value_of("FEATURE").unwrap()) {
+                }
+            },
+            0u16,
+            false,
+        )
+    } else if let Some(feature_arg) = matches.subcommand_matches("set-vcp") {
+        (
+            match parse::<u8>(feature_arg.value_of("FEATURE").unwrap()) {
                 Ok(num) => num,
                 Err(e) => {
                     eprintln!("Invalid argument for set-vcp: {}", e);
                     std::process::exit(1)
-                },
+                }
             },
-             match parse::<u16>(feature_arg.value_of("VALUE").unwrap()) {
-                 Ok(num) => num,
-                 Err(e) => {
-                     eprintln!("Invalid argument for set-vcp: {}", e);
-                     std::process::exit(1)
-                 },
-             }, true)
-        } else {panic!("Invalid arguments! Check the argument parsing logic.")};
+            match parse::<u16>(feature_arg.value_of("VALUE").unwrap()) {
+                Ok(num) => num,
+                Err(e) => {
+                    eprintln!("Invalid argument for set-vcp: {}", e);
+                    std::process::exit(1)
+                }
+            },
+            true,
+        )
+    } else {
+        panic!("Invalid arguments! Check the argument parsing logic.")
+    };
 
     let mut displays = Display::enumerate();
 
-    let display =
-        if monitor <= displays.len() {
-            &mut displays[monitor - 1]
-        } else {
-            eprintln!("Monitor number {} not available.", monitor);
-            std::process::exit(1)
-        };
+    let display = if monitor <= displays.len() {
+        &mut displays[monitor - 1]
+    } else {
+        eprintln!("Monitor number {} not available.", monitor);
+        std::process::exit(1)
+    };
 
     if write {
         display.handle.set_vcp_feature(feature, value).unwrap();
